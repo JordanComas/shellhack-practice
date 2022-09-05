2299ef1ce5962764ad289a7d46bbe0a6c93ae9d8
JordanComas <jordancomas1@yahoo.com>
2022-09-05 12:43:24 -0400

    more changes to home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,12 @@ import React from "react";
 import "..Home/home.css";
 
 const Home = () => {
-  return <div>This is the home page</div>;
+  return (
+    <div>
+      This is the home page
+      <h1>Starting Home page now</h1>
+    </div>
+  );
 };
 
 export default Home;
