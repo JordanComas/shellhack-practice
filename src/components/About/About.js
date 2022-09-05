38e63d2f2cf5af936445d7ecb9350292f58af3a1
Nelson Saroza <nelsonsaroza11@gmail.com>
2022-09-05 12:27:26 -0400

    nelson

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import "..About/about.css";
 
 const About = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>TEST</h1>
+    </div>
+  );
 };
 
 export default About;
